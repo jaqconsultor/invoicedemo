4e4ad847a2b3fbc5c0ed29113c20c1f38987dea1
Jesus Aquino <jaqconsultor@gmail.com>
2023-11-23 22:52:25 -0400

    jj

--- src/main/java/org/openxava/invoicedemo/run/invoicedemo.java
+++ src/main/java/org/openxava/invoicedemo/run/invoicedemo.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class invoicedemo {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("invoicedemo-db"); // To use your own database comment this line and configure web/META-INF/context.xml
+		//DBServer.start("invoicedemo-db"); // To use your own database comment this line and configure web/META-INF/context.xml
 		AppServer.run("invoicedemo"); // Use AppServer.run("") to run in root context
 	}
 
